eb96d63eb2ea63e81589d88b9ba30748c2b56342
njunwa1 <njunwavictor@gmail.com>
2023-09-12 20:16:37 +0300

    adds Variation composable

---
+++ src/composables/variations.js
@@ -0,0 +1,122 @@
+import { reactive, ref} from "vue";
+import router from "../router";
+
+
+export default function useVariations() {
+    const variations = ref([]);
+    const variation = ref({});
+    const errors = ref({})
+    const isLoading = ref(false)
+    const isFetching = ref(false)
+    const paginationMetaData = ref({})
+    const paginationLinks = ref({})
+    const variationsURL = ref('api/variations')
+
+    const variationForm = reactive(
+        {
+            name: '',
+            price: '',
+            is_incrementing: true,
+            type: 'button'
+        }
+    )
+
+    const getVariations = async (searchName) => {
+        isFetching.value = true
+        await axios.get(variationsURL.value+'?name='+searchName).then(response =>{
+            variations.value = response.data.data
+            paginationMetaData.value = response.data.meta
+            paginationLinks.value = response.data.links
+        }).catch(error =>{
+            Toast.fire({
+                icon: 'error',
+                title: error.message
+            })
+        }).finally(
+            () => isFetching.value = false
+        )
+    }
+
+    const getVariation = async (id) => {
+        isFetching.value = true
+        await axios.get('/api/variations/'+id).then(response =>{
+            variation.value = response.data.data
+        }).catch(error =>{
+            Toast.fire({
+                icon: 'error',
+                title: error.message
+            })
+        }).finally(
+            () => isFetching.value = false
+        )
+    }
+
+
+    const storeVariation = async (data) => {
+        isLoading.value = true;
+
+        await axios.post('/api/variations', data)
+            .then(response => {
+                variation.value = response.data.data
+                router.push({name: 'variations.index'})
+                Toast.fire({
+                    icon: 'success',
+                    title: response.data.message
+                })
+            }).catch(error => {
+                if (error.response?.data) {
+                    errors.value = error.response.data.errors
+                } else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: error.message
+                    })
+                }
+            }).finally(
+                () => isLoading.value = false
+            )
+    }
+    const updateVariation = async (id) => {
+
+        isLoading.value = true
+
+        const data = {...variation}
+
+        await axios.patch('/api/variations/' + id, data)
+            .then(response => {
+                router.push({name: 'variations.index'})
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Variation Updated successfully'
+                })
+            }).catch(error => {
+                if (error.response?.data) {
+                    errors.value = error.response.data.errors
+                } else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: error.message
+                    })
+                }
+            }).finally(
+                () => isLoading.value = false
+            )
+    }
+
+
+    return {
+        errors,
+        isLoading,
+        isFetching,
+        variation,
+        variations,
+        getVariation,
+        getVariations,
+        variationForm,
+        storeVariation,
+        updateVariation,
+        paginationLinks,
+        paginationMetaData,
+    }
+
+}
